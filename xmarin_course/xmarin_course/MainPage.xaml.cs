fdb51a129654fbeb991ed5c41774365a45db3c72
K1selev <serge2000k@gmail.com>
2024-09-28 21:03:49 +0300

    make simple widgets with action

--- xmarin_course/xmarin_course/MainPage.xaml.cs
+++ xmarin_course/xmarin_course/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,36 @@ namespace xmarin_course
         public MainPage()
         {
             InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+
+        private async void OnButtonClick(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Click!");
+            var isLightTheme = await DisplayAlert("Выбор темы", "Выборите тему", "Светлая", "Не фильтрованная");
+            if (isLightTheme)
+            {
+                UseLightMode();
+            }
+            else
+            {
+                UseDarkMode();
+            }
+        }
+
+        private void UseLightMode()
+        {
+            Debug.WriteLine("choose light mode");
+            Application.Current.UserAppTheme = OSAppTheme.Light;
+            label.Text = "Выбрана светлая тема";
+        }
+
+        private void UseDarkMode()
+        {
+            Debug.WriteLine("choose dark mode");
+            Application.Current.UserAppTheme = OSAppTheme.Dark;
+            label.Text = "Выбрана темная тема";
         }
     }
 }
